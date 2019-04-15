2eb705f785f23903a3a3c4e2b867472a46557306
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-04-15 16:11:11 +0200

    created basic project structure

---
+++ src/main/java/com/github/maleksandrowicz93/gameofthroneslcgdeckbuilder/GameOfThronesLcgDeckBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.gameofthroneslcgdeckbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfThronesLcgDeckBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfThronesLcgDeckBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maleksandrowicz93/gameofthroneslcgdeckbuilder/GameOfThronesLcgDeckBuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.maleksandrowicz93.gameofthroneslcgdeckbuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameOfThronesLcgDeckBuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
